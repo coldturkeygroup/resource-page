8c8474115d4523fe5a21b3462a6935d20f269973
Aaron Huisinga <aaron@platform.marketing>
2017-06-16 11:12:08 -0500

    Bump to v1.1.3
    1. Use https when creating campaigns in Platform CRM

--- classes/class-resource-page.php
+++ classes/class-resource-page.php
@@ -2,7 +2,9 @@
 
 namespace ColdTurkey\ResourcePage;
 
-if (!defined('ABSPATH')) exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly.
 
 // Composer autoloader
 require_once RESOURCE_PAGE_PLUGIN_PATH . 'vendor/autoload.php';
@@ -83,41 +85,42 @@ class ResourcePage
     public function register_post_type()
     {
         $labels = [
-            'name' => _x('Resource Page', 'post type general name', $this->token),
-            'singular_name' => _x('Resource Page', 'post type singular name', $this->token),
-            'add_new' => _x('Add New', $this->token, $this->token),
-            'add_new_item' => sprintf(__('Add New %s', $this->token), __('Resource Page', $this->token)),
-            'edit_item' => sprintf(__('Edit %s', $this->token), __('Resource Page', $this->token)),
-            'new_item' => sprintf(__('New %s', $this->token), __('Resource Page', $this->token)),
-            'all_items' => sprintf(__('All %s', $this->token), __('Resource Page', $this->token)),
-            'view_item' => sprintf(__('View %s', $this->token), __('Resource Page', $this->token)),
-            'search_items' => sprintf(__('Search %a', $this->token), __('Resource Page', $this->token)),
-            'not_found' => sprintf(__('No %s Found', $this->token), __('Resource Page', $this->token)),
+            'name'               => _x('Resource Page', 'post type general name', $this->token),
+            'singular_name'      => _x('Resource Page', 'post type singular name', $this->token),
+            'add_new'            => _x('Add New', $this->token, $this->token),
+            'add_new_item'       => sprintf(__('Add New %s', $this->token), __('Resource Page', $this->token)),
+            'edit_item'          => sprintf(__('Edit %s', $this->token), __('Resource Page', $this->token)),
+            'new_item'           => sprintf(__('New %s', $this->token), __('Resource Page', $this->token)),
+            'all_items'          => sprintf(__('All %s', $this->token), __('Resource Page', $this->token)),
+            'view_item'          => sprintf(__('View %s', $this->token), __('Resource Page', $this->token)),
+            'search_items'       => sprintf(__('Search %a', $this->token), __('Resource Page', $this->token)),
+            'not_found'          => sprintf(__('No %s Found', $this->token), __('Resource Page', $this->token)),
             'not_found_in_trash' => sprintf(__('No %s Found In Trash', $this->token), __('Resource Page', $this->token)),
-            'parent_item_colon' => '',
-            'menu_name' => __('Resource Page', $this->token)
+            'parent_item_colon'  => '',
+            'menu_name'          => __('Resource Page', $this->token)
         ];
 
         $slug = __('resource-page', $this->token);
         $custom_slug = get_option($this->token . '_slug');
-        if ($custom_slug && strlen($custom_slug) > 0 && $custom_slug != '')
+        if ($custom_slug && strlen($custom_slug) > 0 && $custom_slug != '') {
             $slug = $custom_slug;
+        }
 
         $args = [
-            'labels' => $labels,
-            'public' => true,
-            'publicly_queryable' => true,
+            'labels'              => $labels,
+            'public'              => true,
+            'publicly_queryable'  => true,
             'exclude_from_search' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'query_var' => true,
-            'rewrite' => ['slug' => $slug],
-            'capability_type' => 'post',
-            'has_archive' => false,
-            'hierarchical' => false,
-            'supports' => ['title', 'thumbnail'],
-            'menu_position' => 5,
-            'menu_icon' => 'dashicons-admin-calendar'
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'query_var'           => true,
+            'rewrite'             => ['slug' => $slug],
+            'capability_type'     => 'post',
+            'has_archive'         => false,
+            'hierarchical'        => false,
+            'supports'            => ['title', 'thumbnail'],
+            'menu_position'       => 5,
+            'menu_icon'           => 'dashicons-admin-calendar'
         ];
 
         register_post_type($this->token, $args);
@@ -166,15 +169,15 @@ class ResourcePage
         global $post, $post_ID;
 
         $messages[$this->token] = [
-            0 => '', // Unused. Messages start at index 1.
-            1 => sprintf(__('Page updated. %sView page%s.', $this->token), '<a href="' . esc_url(get_permalink($post_ID)) . '">', '</a>'),
-            4 => __('Resource Page updated.', $this->token),
+            0  => '', // Unused. Messages start at index 1.
+            1  => sprintf(__('Page updated. %sView page%s.', $this->token), '<a href="' . esc_url(get_permalink($post_ID)) . '">', '</a>'),
+            4  => __('Resource Page updated.', $this->token),
             /* translators: %s: date and time of the revision */
-            5 => isset($_GET['revision']) ? sprintf(__('Resource Page restored to revision from %s.', $this->token), wp_post_revision_title((int)$_GET['revision'], false)) : false,
-            6 => sprintf(__('Resource Page published. %sView Page%s.', $this->token), '<a href="' . esc_url(get_permalink($post_ID)) . '">', '</a>'),
-            7 => __('Resource Page saved.', $this->token),
-            8 => sprintf(__('Resource Page submitted. %sPreview Page%s.', $this->token), '<a target="_blank" href="' . esc_url(add_query_arg('preview', 'true', get_permalink($post_ID))) . '">', '</a>'),
-            9 => sprintf(__('Resource Page scheduled for: %1$s. %2$sPreview Page%3$s.', $this->token), '<strong>' . date_i18n(__('M j, Y @ G:i', $this->token), strtotime($post->post_date)) . '</strong>', '<a target="_blank" href="' . esc_url(get_permalink($post_ID)) . '">', '</a>'),
+            5  => isset($_GET['revision']) ? sprintf(__('Resource Page restored to revision from %s.', $this->token), wp_post_revision_title((int) $_GET['revision'], false)) : false,
+            6  => sprintf(__('Resource Page published. %sView Page%s.', $this->token), '<a href="' . esc_url(get_permalink($post_ID)) . '">', '</a>'),
+            7  => __('Resource Page saved.', $this->token),
+            8  => sprintf(__('Resource Page submitted. %sPreview Page%s.', $this->token), '<a target="_blank" href="' . esc_url(add_query_arg('preview', 'true', get_permalink($post_ID))) . '">', '</a>'),
+            9  => sprintf(__('Resource Page scheduled for: %1$s. %2$sPreview Page%3$s.', $this->token), '<strong>' . date_i18n(__('M j, Y @ G:i', $this->token), strtotime($post->post_date)) . '</strong>', '<a target="_blank" href="' . esc_url(get_permalink($post_ID)) . '">', '</a>'),
             10 => sprintf(__('Resource Page draft updated. %sPreview Page%s.', $this->token), '<a target="_blank" href="' . esc_url(add_query_arg('preview', 'true', get_permalink($post_ID))) . '">', '</a>'),
         ];
 
@@ -221,8 +224,9 @@ class ResourcePage
 
         $html = '';
 
-        if ($meta['args']['type'] == 'basic')
+        if ($meta['args']['type'] == 'basic') {
             $html .= '<input type="hidden" name="' . $this->token . '_nonce" id="' . $this->token . '_nonce" value="' . wp_create_nonce(plugin_basename($this->dir)) . '">';
+        }
 
         if (0 < count($field_data)) {
             $html .= '<table class="form-table">' . "\n";
@@ -234,8 +238,9 @@ class ResourcePage
                 $data = $v['default'];
                 $placeholder = $v['placeholder'];
                 $type = $v['type'];
-                if (isset($fields[$k]) && isset($fields[$k][0]))
+                if (isset($fields[$k]) && isset($fields[$k][0])) {
                     $data = $fields[$k][0];
+                }
 
                 if ($type == 'text') {
                     $html .= '<tr valign="top"><th scope="row"><label for="' . esc_attr($k) . '">' . $v['name'] . '</label></th><td>';
@@ -251,15 +256,16 @@ class ResourcePage
                     global $post;
                     $args = [
                         'posts_per_page' => 20,
-                        'post_type' => $v['default'],
-                        'post_status' => 'publish'
+                        'post_type'      => $v['default'],
+                        'post_status'    => 'publish'
                     ];
                     $custom_posts = get_posts($args);
                     foreach ($custom_posts as $post) : setup_postdata($post);
                         $link = str_replace(home_url(), '', get_permalink());
                         $selected = '';
-                        if ($link == $data)
+                        if ($link == $data) {
                             $selected = 'selected';
+                        }
 
                         $html .= '<option value="' . $link . '" ' . $selected . '>' . get_the_title() . '</option>';
                     endforeach;
@@ -272,8 +278,9 @@ class ResourcePage
                     $html .= '<select style="width:100%" name="' . esc_attr($k) . '" id="' . esc_attr($k) . '">';
                     foreach ($v['options'] as $option) {
                         $selected = '';
-                        if ($option == $data)
+                        if ($option == $data) {
                             $selected = 'selected';
+                        }
 
                         $html .= '<option value="' . $option . '" ' . $selected . '>' . ucfirst($option) . '</option>';
                     }
@@ -320,15 +327,18 @@ class ResourcePage
     public function meta_box_save($post_id)
     {
         // Verify
-        if ((get_post_type() != $this->token) || !wp_verify_nonce($_POST[$this->token . '_nonce'], plugin_basename($this->dir)))
+        if ((get_post_type() != $this->token) || !wp_verify_nonce($_POST[$this->token . '_nonce'], plugin_basename($this->dir))) {
             return $post_id;
+        }
 
         if ('page' == $_POST['post_type']) {
-            if (!current_user_can('edit_page', $post_id))
+            if (!current_user_can('edit_page', $post_id)) {
                 return $post_id;
+            }
         } else {
-            if (!current_user_can('edit_post', $post_id))
+            if (!current_user_can('edit_post', $post_id)) {
                 return $post_id;
+            }
         }
 
         $field_data = $this->get_custom_fields_settings('all');
@@ -344,8 +354,9 @@ class ResourcePage
             }
 
             // Escape the URLs.
-            if ('url' == $field_data[$f]['type'])
+            if ('url' == $field_data[$f]['type']) {
                 ${$f} = esc_url(${$f});
+            }
             if (${$f} == '') {
                 delete_post_meta($post_id, $f, get_post_meta($post_id, $f, true));
             } else {
@@ -400,57 +411,57 @@ class ResourcePage
 
         if ($meta_box == 'basic' || $meta_box == 'all') {
             $fields['background_image'] = [
-                'name' => __('Background Image', $this->token),
+                'name'        => __('Background Image', $this->token),
                 'description' => __('The background image for the page.', $this->token),
                 'placeholder' => '',
-                'type' => 'url',
-                'default' => '',
-                'section' => 'info'
+                'type'        => 'url',
+                'default'     => '',
+                'section'     => 'info'
             ];
 
             $fields['modal_title'] = [
-                'name' => __('Modal Title', $this->token),
+                'name'        => __('Modal Title', $this->token),
                 'description' => __('The title of the modal popup.', $this->token),
                 'placeholder' => 'Get Instant Access!',
-                'type' => 'text',
-                'default' => 'Get Instant Access!',
-                'section' => 'info'
+                'type'        => 'text',
+                'default'     => 'Get Instant Access!',
+                'section'     => 'info'
             ];
 
             $fields['modal_cta'] = [
-                'name' => __('Modal CTA', $this->token),
+                'name'        => __('Modal CTA', $this->token),
                 'description' => __('The text of the submit button on the modal popup.', $this->token),
                 'placeholder' => 'Watch The Videos!',
-                'type' => 'text',
-                'default' => 'Watch The Videos!',
-                'section' => 'info'
+                'type'        => 'text',
+                'default'     => 'Watch The Videos!',
+                'section'     => 'info'
             ];
 
             $fields['legal_broker'] = [
-                'name' => __('Your Legal Broker', $this->token),
+                'name'        => __('Your Legal Broker', $this->token),
                 'description' => __('This will be displayed on the bottom of the page.', $this->token),
                 'placeholder' => '',
-                'type' => 'text',
-                'default' => '',
-                'section' => 'info'
+                'type'        => 'text',
+                'default'     => '',
+                'section'     => 'info'
             ];
 
             $fields['primary_color'] = [
-                'name' => __('Primary Color', $this->token),
+                'name'        => __('Primary Color', $this->token),
                 'description' => __('Change the primary color of the funnel page.', $this->token),
                 'placeholder' => '',
-                'type' => 'color',
-                'default' => '',
-                'section' => 'info'
+                'type'        => 'color',
+                'default'     => '',
+                'section'     => 'info'
             ];
 
             $fields['hover_color'] = [
-                'name' => __('Hover Color', $this->token),
+                'name'        => __('Hover Color', $this->token),
                 'description' => __('Change the button hover color of the funnel page.', $this->token),
                 'placeholder' => '',
-                'type' => 'color',
-                'default' => '',
-                'section' => 'info'
+                'type'        => 'color',
+                'default'     => '',
+                'section'     => 'info'
             ];
         }
 
@@ -458,21 +469,21 @@ class ResourcePage
             $count = 1;
             while ($count <= 5) {
                 $fields['video_' . $count . '_title'] = [
-                    'name' => __('Video ' . $count . ' Title', $this->token),
+                    'name'        => __('Video ' . $count . ' Title', $this->token),
                     'description' => __('The title for your video.', $this->token),
                     'placeholder' => '',
-                    'type' => 'text',
-                    'default' => '',
-                    'section' => 'info'
+                    'type'        => 'text',
+                    'default'     => '',
+                    'section'     => 'info'
                 ];
 
                 $fields['video_' . $count . '_url'] = [
-                    'name' => __('Video ' . $count . ' Embed URL', $this->token),
+                    'name'        => __('Video ' . $count . ' Embed URL', $this->token),
                     'description' => __('The embed code for your video.', $this->token),
                     'placeholder' => '',
-                    'type' => 'text',
-                    'default' => '',
-                    'section' => 'info'
+                    'type'        => 'text',
+                    'default'     => '',
+                    'section'     => 'info'
                 ];
 
                 $count++;
@@ -481,12 +492,12 @@ class ResourcePage
 
         if ($meta_box == 'marketing' || $meta_box == 'all') {
             $fields['retargeting'] = [
-                'name' => __('Facebook Pixel - Retargeting (optional)', $this->token),
+                'name'        => __('Facebook Pixel - Retargeting (optional)', $this->token),
                 'description' => __('Facebook Pixel to allow retargeting of people that view this page.', $this->token),
                 'placeholder' => __('Ex: 4123423454', $this->token),
-                'type' => 'text',
-                'default' => '',
-                'section' => 'info'
+                'type'        => 'text',
+                'default'     => '',
+                'section'     => 'info'
             ];
         }
 
@@ -523,8 +534,9 @@ class ResourcePage
         if ($pageID) {
             $file = get_post_meta($pageID, 'media_file', true);
 
-            if (preg_match('/(\.jpg|\.png|\.bmp|\.gif)$/', $file))
+            if (preg_match('/(\.jpg|\.png|\.bmp|\.gif)$/', $file)) {
                 return '<img src="' . $file . '" style="margin-left:auto;margin-right:auto;margin-bottom:0px;display:block;" class="img-responsive img-thumbnail">';
+            }
         }
 
         return false;
@@ -554,8 +566,10 @@ class ResourcePage
             $options_table = $wpdb->base_prefix . $blog_id . '_' . 'options';
             $mapped = $wpdb->get_var("SELECT domain FROM {$wpdb->dmtable} WHERE blog_id = '{$blog_id}' ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1");
             $domain = $wpdb->get_var("SELECT option_value FROM {$options_table} WHERE option_name = 'siteurl' LIMIT 1");
-            if ($mapped)
-                $permalink = str_replace($domain, 'http://' . $mapped, $permalink);
+
+            if ($mapped) {
+                $permalink = str_replace($domain, 'https://' . $mapped, $permalink);
+            }
         }
 
         if (($_POST['post_status'] != 'publish') || ($_POST['original_post_status'] == 'publish')) {
@@ -563,6 +577,7 @@ class ResourcePage
             if ($campaign_id != '' && is_int($campaign_id)) {
                 $this->crm->updateCampaign($campaign_id, get_the_title($post_ID), $permalink);
             }
+
             return true;
         }
 
@@ -590,8 +605,8 @@ class ResourcePage
             // Create the prospect on Platform CRM
             $this->crm->createProspect([
                 'campaign_id' => $platform_crm_campaign,
-                'first_name' => $first_name,
-                'email' => $email
+                'first_name'  => $first_name,
+                'email'       => $email
             ]);
 
             echo json_encode(['status' => 'success']);
